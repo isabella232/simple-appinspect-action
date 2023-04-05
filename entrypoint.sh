7cbac61705477ac40547a777d953ef232683e8ea
mbruzda <mbruzda@splunk.com>
2023-04-05 10:03:20 +0200

    fix: addon name path

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-ADDON_NAME=$(ls /github/workflow/$INPUT_APP_PATH)
+ADDON_NAME=$(ls $INPUT_APP_PATH)
 ADDON_FULL_PATH="$INPUT_APP_PATH/$ADDON_NAME"
 
-python3 /run_appinspect.py $INPUT_USERNAME $INPUT_PASSWORD $ADDON_FULL_PATH
+python3 /run_appinspect.py a$INPUT_USERNAME $INPUT_PASSWORD $ADDON_FULL_PATH
